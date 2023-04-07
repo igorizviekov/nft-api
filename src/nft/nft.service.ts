2931f721ce1eebbd52e90bdc122886606f1add52
Igor Izviekov <igorizviekov@Igors-MacBook-Pro-2.local>
2023-04-07 10:58:27 +0300

    refactor: error catch on create NFT

--- src/nft/nft.service.ts
+++ src/nft/nft.service.ts
@@ -6,94 +6,104 @@ import { fetchContract } from "src/utils";
 import { Nft } from "./nft.entity";
 import { MarketAddress } from "src/constants/constants";
 import { IResponse } from "src/app.types";
+import { HttpException, HttpStatus } from "@nestjs/common";
+
 @Injectable()
 export class NftService {
   constructor(private configService: ConfigService) {}
 
   async mintNft(file, price: number, metadata): Promise<IResponse> {
-    const { buffer } = file;
+    try {
+      const { buffer } = file;
 
-    /**
-     * Authenticate to Infura
-     *
-     * https://app.infura.io/
-     * https://ipfs.tech/
-     */
-    const auth =
-      "Basic " +
-      Buffer.from(
-        `${this.configService.get(
-          "INFURA_PROJECT_ID"
-        )}:${this.configService.get("INFURA_SECRET")}`
-      ).toString("base64");
+      /**
+       * Authenticate to Infura
+       *
+       * https://app.infura.io/
+       * https://ipfs.tech/
+       */
+      const auth =
+        "Basic " +
+        Buffer.from(
+          `${this.configService.get(
+            "INFURA_PROJECT_ID"
+          )}:${this.configService.get("INFURA_SECRET")}`
+        ).toString("base64");
 
-    const options = {
-      url: "https://ipfs.infura.io:5001/api/v0",
-      host: "ipfs.infura.io",
-      port: 5001,
-      protocol: "https",
-      apiPath: "/api/v0",
-      headers: {
-        authorization: auth,
-      },
-    };
-    const client = ipfsClient(options);
+      const options = {
+        url: "https://ipfs.infura.io:5001/api/v0",
+        host: "ipfs.infura.io",
+        port: 5001,
+        protocol: "https",
+        apiPath: "/api/v0",
+        headers: {
+          authorization: auth,
+        },
+      };
+      const client = ipfsClient(options);
 
-    /**
-     * Upload file to Infura
-     */
-    const addedImage = await client.add({
-      content: buffer,
-    });
-    /**
-     * Upload NFT data to Infura
-     */
-    const data = JSON.stringify({
-      price,
-      // url of Infura project plus id of uploaded image
-      image: `https://${this.configService.get(
+      /**
+       * Upload file to Infura
+       */
+      const addedImage = await client.add({
+        content: buffer,
+      });
+      /**
+       * Upload NFT data to Infura
+       */
+      const data = JSON.stringify({
+        price,
+        // url of Infura project plus id of uploaded image
+        image: `https://${this.configService.get(
+          "INFURA_PROJECT_NAME"
+        )}.infura-ipfs.io/ipfs/${addedImage.path}`,
+        metadata: JSON.parse(metadata as unknown as string),
+      });
+
+      /**
+       * Upload file to Infura
+       */
+      const addedNFT = await client.add(data);
+
+      const nftURL = `https://${this.configService.get(
         "INFURA_PROJECT_NAME"
-      )}.infura-ipfs.io/ipfs/${addedImage.path}`,
-      metadata: JSON.parse(metadata as unknown as string),
-    });
+      )}.infura-ipfs.io/ipfs/${addedNFT.path}`;
 
-    /**
-     * Upload file to Infura
-     */
-    const addedNFT = await client.add(data);
+      const provider = new ethers.providers.JsonRpcProvider();
 
-    const nftURL = `https://${this.configService.get(
-      "INFURA_PROJECT_NAME"
-    )}.infura-ipfs.io/ipfs/${addedNFT.path}`;
+      /**
+       * Person who is creating an NFT
+       */
+      const signer = provider.getSigner();
 
-    const provider = new ethers.providers.JsonRpcProvider();
+      /**
+       * Get access to the Solidity Smart Contract api
+       */
+      const contract = fetchContract(signer);
 
-    /**
-     * Person who is creating an NFT
-     */
-    const signer = provider.getSigner();
+      /**
+       * Convert price value from the form input to the blockchain readable format
+       */
+      const bPrice = ethers.utils.parseUnits(price.toString(), "ether");
+      const listingPrice = await contract.getListingPrice();
+      const transaction = await contract.createToken(bPrice, nftURL, {
+        value: listingPrice,
+      });
 
-    /**
-     * Get access to the Solidity Smart Contract api
-     */
-    const contract = fetchContract(signer);
+      await transaction.wait();
+      const nft = new Nft();
 
-    /**
-     * Convert price value from the form input to the blockchain readable format
-     */
-    const bPrice = ethers.utils.parseUnits(price.toString(), "ether");
-    const listingPrice = await contract.getListingPrice();
-    const transaction = await contract.createToken(bPrice, nftURL, {
-      value: listingPrice,
-    });
+      nft.price = price;
+      nft.contractAddress = MarketAddress;
+      nft.metadata = metadata as unknown as string;
 
-    await transaction.wait();
-    const nft = new Nft();
-
-    nft.price = price;
-    nft.contractAddress = MarketAddress;
-    nft.metadata = metadata as unknown as string;
-
-    return { status: "success", data: nft };
+      return { status: "success", data: nft };
+    } catch (e) {
+      console.log(e);
+      throw new HttpException(
+        "Error creating new NFT",
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
   }
 }
