3da0db1514825faf4edb27beb6e328ccfd5d531a
igorizviekov <igorizviekov@gmail.com>
2023-05-05 09:33:19 +0300

    fix: remove Joi validation

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -1,11 +1,11 @@
 import * as Joi from "@hapi/joi";
 
 export const ConfigValidationSchema = Joi.object({
-  DB_HOST: Joi.string().required(),
-  DB_USERNAME: Joi.string().required(),
-  DB_PASSWORD: Joi.string().required(),
-  DB_DATABASE: Joi.string().required(),
-  DB_PORT: Joi.number().default(5432).required(),
-  JWT_SECRET: Joi.string().required(),
-  JWT_EXPIRES: Joi.number().default(3600).required(),
+  // DB_HOST: Joi.string().required(),
+  // DB_USERNAME: Joi.string().required(),
+  // DB_PASSWORD: Joi.string().required(),
+  // DB_DATABASE: Joi.string().required(),
+  // DB_PORT: Joi.number().default(5432).required(),
+  // JWT_SECRET: Joi.string().required(),
+  // JWT_EXPIRES: Joi.number().default(3600).required(),
 });
