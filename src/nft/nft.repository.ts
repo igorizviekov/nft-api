2c36e499e9abedc48546e591efb2db9b1af29196
igorizviekov <igorizviekov@gmail.com>
2023-06-22 15:31:32 +0300

    refactor: removed unused modules

--- src/nft/nft.repository.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { NftEntity } from "./nft.entity";
-
-@EntityRepository(NftEntity)
-export class NftRepository extends Repository<NftEntity> {
-  // Define your custom repository methods here
-}
